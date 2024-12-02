2c6c36822af067d43b26ab49eb10c18b8bca9a6e
MrSpee <70175396+MrSpee@users.noreply.github.com>
2024-12-02 22:02:05 +0100

    Analytics add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import {Header} from '../components/Header'
 import {Footer} from '../components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="flex-grow flex flex-col items-center justify-start pt-4">
           {children}
         </main>
+        <Analytics /> 
         <Footer />
       </body>
     </html>
